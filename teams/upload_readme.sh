f74eee0e7f7ff9c96d4703e58196f64c902ebbb3
Alex Ankudinov <xelez0@gmail.com>
2021-12-12 03:11:03 +0500

    upload renamed readme

--- teams/upload_readme.sh
+++ teams/upload_readme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for (( i = 101; i < 105; i += 1 )) ; do
-    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null TEAM_README ctfcup@10.118.$i.10:README
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null TEAM_README.md ctfcup@10.118.$i.10:README.md
 done
